aa59dbedcb6160a93c88da1621380f05c052c5d1
Lowell List <lowell.list@gmail.com>
2018-01-02 14:30:32 -0800

    updated deploy script not to clobber entire base path

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
+#!/usr/bin/env bash
+
 printf "syncing to Amazon S3...\n"
-aws s3 sync ./ s3://nile.babasmg.com \
+aws s3 sync ./public s3://nile.babasmg.com/webapp/ \
   --region us-west-2 \
-  --exclude ".git/*" \
   --exclude ".DS_Store" \
-  --exclude "deploy.sh" \
   --delete
 printf "done.\n"
