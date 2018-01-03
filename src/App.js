be0542b755ee1c64a6ea7948008b37068682ac26
Lowell List <lowell.list@gmail.com>
2018-01-03 10:35:26 -0800

    first page, with custom fonts

--- src/App.js
+++ src/App.js
@@ -16,13 +16,19 @@ class App extends Component {
         {/*<p className="App-intro">*/}
           {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
         {/*</p>*/}
-        <Page11 test={"testing"}/>
+        <Page11 onStartClick={() => { console.log("button clicked."); } }/>
       </div>
     );
   }
 }
 
 class Page11 extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log("page props: " + props);
+  }
+
   render() {
     return (
       <div className="Page">
@@ -30,9 +36,13 @@ class Page11 extends Component {
         <div className="Page-video-wrapper">
           <iframe title="introVideo" className="Page-video" src="https://www.youtube.com/embed/yFw5KORObxA" frameBorder="0" gesture="media" allow="encrypted-media" allowFullScreen/>
         </div>
-        <p>Let us help make your dream come true!</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <button className="Page-next-button">Start!</button>
+        <p className="Page-Header-1">Let us help make your dream come true!</p>
+        <p className="Page-normal-text">Lorem ipsum dolor sit amet, ea vim viris detraxit instructior. Vim liber iudico cetero an, ex vidit noluisse cum, ei nam nibh invidunt. Cum id erat regione integre. Per ei luptatum atomorum, an brute constituto conclusionemque sed.</p>
+        <p className="Page-normal-text">Causae delectus gubergren an vis. Moderatius consequuntur vel ex. Ut mel nisl aliquando, an usu probo lorem graeco, sit ex denique argumentum. Eu simul veritus percipit nam. Qui ut mutat iusto. Sit utinam aliquip fuisset id, utroque verterem ius ad.</p>
+        <p className="Page-normal-text">Eos populo delenit repudiandae id, in eripuit imperdiet mel, iuvaret dolores vis id. Menandri scripserit sed in, eam in mollis expetenda repudiandae. Ne eros error hendrerit mea. Ut usu libris virtute. At enim falli accommodare vis.</p>
+        <p className="Page-normal-text">Ut modo aeterno concludaturque pri, eu voluptua ullamcorper sit. Eu sea elitr constituto, vel cibo alterum inermis te. Omnium nostrum ne mea. Ut vis summo choro animal, pri cu ullum cetero eripuit.</p>
+        <p className="Page-normal-text">Mea eu augue omnium timeam, eu salutatus disputationi vel, id wisi saepe delicata has. An eum odio inermis. Quo nibh intellegat interesset an. Soluta prompta cu per, ut quidam bonorum epicurei sit.</p>
+        <button className="Page-next-button Page-evergreen-button" onClick={this.props.onStartClick}>Start!</button>
       </div>
     );
   }
