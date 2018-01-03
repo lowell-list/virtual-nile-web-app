b41d18beb5c9f3574a4379d027747f294bcd24f1
Lowell List <lowell.list@gmail.com>
2018-01-02 16:51:48 -0800

    initial work

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 printf "syncing to Amazon S3...\n"
-aws s3 sync ./public s3://nile.babasmg.com/webapp/ \
+aws s3 sync ./build s3://nile.babasmg.com/webapp/ \
   --region us-west-2 \
   --exclude ".DS_Store" \
   --delete
--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './Page.css';
+import babas_logo from './img/babas_logo.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/*<header className="App-header">*/}
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          {/*<h1 className="App-title">Welcome to React</h1>*/}
+        {/*</header>*/}
+        {/*<p className="App-intro">testing</p>*/}
+        {/*<p className="App-intro">*/}
+          {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
+        {/*</p>*/}
+        <Page11 test={"testing"}/>
+      </div>
+    );
+  }
+}
+
+class Page11 extends Component {
+  render() {
+    return (
+      <div className="Page">
+        <img src={babas_logo} className="App-babas-logo" alt="babas_logo" />
+        <div className="Page-video-wrapper">
+          <iframe title="introVideo" className="Page-video" src="https://www.youtube.com/embed/yFw5KORObxA" frameBorder="0" gesture="media" allow="encrypted-media" allowFullScreen/>
+        </div>
+        <p>Let us help make your dream come true!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <button className="Page-next-button">Start!</button>
       </div>
     );
   }
