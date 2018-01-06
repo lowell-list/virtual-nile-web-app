4aa793308a0010080a10c56e289e810c6bd22c2d
Lowell List <lowell.list@gmail.com>
2018-01-05 20:24:53 -0800

    'the' to 'a'

--- src/Page.js
+++ src/Page.js
@@ -16,7 +16,7 @@ export class PageLanding extends Component {
           {/*<iframe title="introVideo" className="Page__video" src="https://www.youtube.com/embed/yFw5KORObxA" frameBorder="0" gesture="media" allow="encrypted-media" allowFullScreen/>*/}
         {/*</div>*/}
         <p className="Page__header1 Page__header1--blue">Let us help make your dream come true!</p>
-        <p className="Page__header1 Page__header1--orange">Hope deferred makes the heart sick, but a dream fulfilled is the tree of life.</p>
+        <p className="Page__header1 Page__header1--orange">Hope deferred makes the heart sick, but a dream fulfilled is a tree of life.</p>
         <p className="Page__normalText">According to an ancient Egyptian story, party guests would float a lotus flower with a burning candle placed carefully in the middle.</p>
         <p className="Page__normalText">Each person would share a dream hidden in their heart before releasing the lotus flower down the famous Nile river.</p>
         <p className="Page__normalText">It was believed that if the flame continued to burn for the duration of the meal their dream would eventually come true.</p>
