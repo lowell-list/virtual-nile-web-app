abf7611dd0f9f7b67dec13dbc89854a3514ae366
Lowell List <lowell.list@gmail.com>
2018-01-09 15:54:38 -0800

    moved a couple more colors into Shared.scss; improved page padding consistency; added max-width so page looks OK on desktop

--- src/Page.js
+++ src/Page.js
@@ -162,7 +162,7 @@ export class PageDreamConfirmed extends Component {
       <div className="Page Page__dreamConfirmed">
         <div className="Page__fullBlueBackground"/>
         <img src={babas_logo_small} className="Page__babasLogoSmall" alt="babas_logo_small" />
-        <p className="Page__header1 Page__header1--white">{this.props.confirmationText}</p>
+        <p className="Page__header1 Page__header1--light">{this.props.confirmationText}</p>
         <img src={icon_checkmark} className="Page__iconCheckmark" alt="icon_checkmark" />
         {/*<p className="Page__normalText Page__normalText--white">Lorem ipsum dolor sit amet, ea vim viris detraxit instructior. Vim liber iudico cetero an, ex vidit noluisse cum, ei nam nibh invidunt. Cum id erat regione integre. Per ei luptatum atomorum, an brute constituto conclusionemque sed.</p>*/}
         <br/><br/><br/>
