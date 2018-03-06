7f7df87126294d0e7e83806ccae4e60d18927741
Unknown <lowell.list@gmail.com>
2018-03-06 15:56:29 -0800

    new deploy scripts

---
+++ deploy_test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+printf "syncing to Amazon S3...\n"
+aws s3 sync ./build s3://nile.babasmg.com/webapp-test/ \
+  --region us-west-2 \
+  --exclude ".DS_Store" \
+  --delete
+printf "done.\n"
