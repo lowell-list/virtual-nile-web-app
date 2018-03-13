64ef6a3d36219c81627649b214062a740dd32bee
Unknown <lowell.list@gmail.com>
2018-03-13 16:09:45 -0700

    experimentation with react-touch

--- src/component/PageFlickFlower.js
+++ src/component/PageFlickFlower.js
@@ -1,15 +1,33 @@
 import React, {Component} from 'react';
+import { Draggable } from 'react-touch';
 import NightLotusFlower from './NightLotusFlower';
 
 export default class PageFlickFlower extends Component {
 
   render() {
     return (
-      <div className="Page">
+      <div className="Page"
+           ref={(element) => {
+             element.addEventListener('touchmove', function(e) {
+               e.preventDefault();
+             }, false);
+           }}
+      >
         <p className="Page__header1 Page__header1--blue">
           {this.props.screenName}, "FLICK" your lotus flower into the Virtual Nile
         </p>
-        <NightLotusFlower colors={this.props.colors} />
+
+        <Draggable position={{translateX: 0, translateY: 0}}>
+          {({translateX, translateY}) => {
+            console.log('translation: ' + translateX + ',' + translateY);
+            return (
+              <div style={{transform: `translate3d(0, ${translateY}px, 0)`}}>
+                <NightLotusFlower colors={this.props.colors} />
+              </div>
+            );
+          }}
+        </Draggable>
+
       </div>
     );
   }
